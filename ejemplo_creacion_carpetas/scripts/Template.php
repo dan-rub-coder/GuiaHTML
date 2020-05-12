6bdf31d80382f6137bdc0d0f56ee1d4da33d8c61
DanielCamilo <dcrubiano01@gmail.com>
2020-05-12 09:45:27 -0500

    Firs commit HTML

---
+++ ejemplo_creacion_carpetas/scripts/Template.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+   <html>
+     <head>
+	 <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Zapatos ABC</title>
+     </head>
+     <body>
+     Los Zapatos ABC llegan al mercado con innovación y calidad
+     </body>
+   </html>
